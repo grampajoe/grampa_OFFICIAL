af475904a5b60103f10bda65d472bda3667ee809
Joe Friedl <joe@joefriedl.net>
2014-06-28 15:26:41 -0400

    Initial commit

---
+++ bots.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'twitter_ebooks'
+
+# This is an example bot definition with event handlers commented out
+# You can define as many of these as you like; they will run simultaneously
+
+Ebooks::Bot.new("grampa_OFFICIAL") do |bot|
+  # Consumer details come from registering an app at https://dev.twitter.com/
+  # OAuth details can be fetched with https://github.com/marcel/twurl
+  bot.consumer_key = ENV['CONSUMER_KEY']
+  bot.consumer_secret = ENV['CONSUMER_SECRET']
+  bot.oauth_token = ENV['OAUTH_TOKEN']
+  bot.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
+
+  model = Ebooks::Model.load("model/grampa_OFFICIAL.model")
+
+  bot.on_message do |dm|
+    # Reply to a DM
+    # bot.reply(dm, "secret secrets")
+  end
+
+  bot.on_follow do |user|
+    # Follow a user back
+    # bot.follow(user[:screen_name])
+  end
+
+  bot.on_mention do |tweet, meta|
+    response = model.make_response(tweet[:text], 130)
+    bot.reply(tweet, meta[:reply_prefix] + response)
+  end
+
+  bot.on_timeline do |tweet, meta|
+    # Reply to a tweet in the bot's timeline
+    # bot.reply(tweet, meta[:reply_prefix] + "nice tweet")
+  end
+
+  bot.scheduler.every ENV['TWEET_INTERVAL'] do
+    statement = model.make_statement()
+    bot.tweet(statement)
+  end
+end
